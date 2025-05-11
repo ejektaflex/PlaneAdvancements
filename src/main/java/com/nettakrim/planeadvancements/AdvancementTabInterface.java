ed5cb687fc6590f5adfb66b6430f38ee9e61ec75
Nettakrim <106409257+Nettakrim@users.noreply.github.com>
2025-05-11 23:10:26 +0100

    grid is now arranged slightly more compactly

--- src/main/java/com/nettakrim/planeadvancements/AdvancementTabInterface.java
+++ src/main/java/com/nettakrim/planeadvancements/AdvancementTabInterface.java
@@ -34,6 +34,7 @@ public interface AdvancementTabInterface {
     }
 
     static void calculateGrid(AdvancementCluster root, List<AdvancementCluster> clusters) {
+        //sort bigger clusters to start of the list
         clusters.sort((a, b) -> {
             int i = Float.compare(b.size.y, a.size.y);
             return i == 0 ? Float.compare(b.size.x, a.size.x) : i;
@@ -44,10 +45,12 @@ public interface AdvancementTabInterface {
 
         IntArrayList mask = new IntArrayList();
 
+        // place clusters in first available spot, stepping left, then up when max width is reached
         for (AdvancementCluster cluster : clusters) {
             int y = 0;
             int search = (maxWidth - cluster.size.x) + 1;
 
+            // if a cluster is too big, just let it overflow
             if (search <= 0) {
                 for (int i = 0; i < cluster.size.y; i++) {
                     mask.add(Integer.MAX_VALUE);
@@ -58,6 +61,7 @@ public interface AdvancementTabInterface {
                 continue;
             }
 
+            // otherwise step through positions
             while (true) {
                 int foundPosition = getFreePos(mask, cluster, search, y);
 
@@ -82,12 +86,56 @@ public interface AdvancementTabInterface {
             }
         }
 
-        // x+y mirror to get 1x1s to the left
+        // settle clusters to what will be the left, this is better than just doing rightwards to begin with as it means bigger clusters will be further to what will be the right
+        for (AdvancementCluster cluster : clusters.reversed()) {
+            int position = (int)cluster.pos.x;
+            int y = (int)cluster.pos.y;
+
+            int startingFilter = ~getMask(cluster.size.x, position);
+
+            while (true) {
+                position++;
+
+                int sizeMask = getMask(cluster.size.x, position);
+
+                boolean fits;
+
+                if (cluster.size.x + position > maxWidth) {
+                    fits = false;
+                } else {
+                    fits = true;
+                    for (int u = 0; u < cluster.size.y; u++) {
+                        int index = y + u;
+                        if ((mask.getInt(index) & startingFilter & sizeMask) != 0) {
+                            fits = false;
+                        }
+                    }
+                }
+
+                if (!fits) {
+                    position--;
+                    if (position == cluster.pos.x) {
+                        break;
+                    }
+
+                    sizeMask = getMask(cluster.size.x, position);
+                    for (int u = 0; u < cluster.size.y; u++) {
+                        int index = y+u;
+                        mask.set(index, (mask.getInt(index) & startingFilter) | sizeMask);
+                    }
+                    cluster.pos.x = position;
+                    break;
+                }
+            }
+        }
+
+        // xy mirror everything
         Vector2f v = new Vector2f(usedWidth+1, mask.size());
         for (AdvancementCluster cluster : clusters) {
             v.sub(cluster.pos, cluster.pos).sub(cluster.size.x, cluster.size.y);
         }
 
+        // set root position, then add it to list so it's position gets applied
         root.pos.x = 0;
         root.pos.y = (mask.size()-1)/2f;
         clusters.add(root);
